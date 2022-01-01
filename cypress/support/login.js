0171d4cdf21fe4ffca2a79f9bab4a15e951cdef6
yohannes <yohannesbog@gmail.com>
2022-01-01 12:12:16 -0500

    secure password and user name

--- cypress/support/login.js
+++ cypress/support/login.js
@@ -1,12 +1,11 @@
-const email = "auto.user+bo-pboq-2qlj@peachfinance.com";
-const password = "hello12345";
+
 
 // login with valid password and username
 Cypress.Commands.add("login", () => {
   cy.visit("https://peach-borrower.peach-dev.finance/login");
   cy.wait("@accessApp");
-  cy.get('[name="email"]').scrollIntoView().type(email, { force: true });
-  cy.get('[name="password"]').scrollIntoView().type(password, { force: true });
+  cy.get('[name="email"]').scrollIntoView().type(Cypress.env('email'), { force: true });
+  cy.get('[name="password"]').scrollIntoView().type(Cypress.env('password'), { force: true });
   cy.get('[data-cy="submit"]').click();
   cy.wait("@loginSuccess");
   cy.url().should("include", "finance/loans");
